739d694cef341e07606b1e97d800b5cf4053167b
Julian Acosta <8892386+julacosta9@users.noreply.github.com>
2020-03-30 23:26:10 -0700

    linting

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,9 +13,13 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
-        res.json({id: result.insertId });
-    });
+    burger.create(
+        ["burger_name", "devoured"],
+        [req.body.burger_name, req.body.devoured],
+        function(result) {
+            res.json({ id: result.insertId });
+        }
+    );
 });
 
 router.put("/api/burgers/:id", function(req, res) {
